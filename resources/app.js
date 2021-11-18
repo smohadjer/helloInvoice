b4ba9a207a4a1155b7666a95175736fcc104f076
Saeid Mohadjer <saeid@fastmail.fm>
2021-11-18 11:10:50 +0100

    adds more invoices

--- resources/app.js
+++ resources/app.js
@@ -1,12 +1,34 @@
-var template, templateData;
+const init = () => {
+  const select = document.querySelector('#invoice');
+  const download = document.querySelector('.download');
+  setEventListeners(select, download);
+  fetchInvoice(select.value);
+};
 
-Promise.all([
-  fetch('invoice-template.hbs').then(response => response.text()).then(data => {template = data}),
-  fetch('invoice-data.json').then(response => response.json()).then(data => {
-    templateData = data
-  })
-]).then(function() {
-  const temp = Handlebars.compile(template)
-  const html = temp(templateData);
-  document.body.innerHTML = html;
-});
+const setEventListeners = (select, download) => {
+  select.addEventListener('change', () => fetchInvoice(select.value));
+  download.addEventListener('click', () => window.print());
+};
+
+const fetchInvoice = (data) => {
+  fetch(data)
+  .then(response => response.json())
+  .then(data => {
+    fetch(data.template)
+    .then(response => response.text())
+    .then(template => {
+      const temp = Handlebars.compile(template)
+      const html = temp(data);
+      document.querySelector('main').innerHTML = html;
+    });
+  });
+};
+
+init();
+
+
+
+
+
+
+
